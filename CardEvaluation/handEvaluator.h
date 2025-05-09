6617e5fec7f37f123e1d545e8f91613803a0d0d7
JakeNFrohlich <jakefrohlich00@gmail.com>
2025-05-08 21:15:32 -0400

    Update handEvaluator.h

--- CardEvaluation/handEvaluator.h
+++ CardEvaluation/handEvaluator.h
@@ -6,10 +6,15 @@
 
 namespace std 
 {
+    // returns the hand type as a string
     string evaluateHand(const vector<int>& hand);
+    // returns the card name (like Ace of Spades_
     string getCardName(int cardValue);
+    // return the hand rank
     int handRank(const std::string& handType);
+    // returns the card numbers in sorted order
     std::vector<int> getSortedRanks(const std::vector<int>& hand);
+    // considers the entire table's hands and returns the index of the player who won the game
     int handWinner(const std::vector<std::vector<int>>& playerHands);
 }
 
